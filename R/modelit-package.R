a41d4964bedd6f193d76e8947da7999ec40e3285
Guyliann Engels <guyliann.engels@umons.ac.be>
2025-07-14 15:10:19 +0200

    To enhance code readability, group multiple importFrom directives from the same package into a single statement.

--- R/modelit-package.R
+++ R/modelit-package.R
@@ -23,20 +23,9 @@
 #' @importFrom broom augment glance tidy
 #' @importFrom chart chart combine_charts
 #' @importFrom data.io label
-#' @importFrom flextable add_footer_lines
-#' @importFrom flextable add_header_lines
-#' @importFrom flextable align
-#' @importFrom flextable as_equation
-#' @importFrom flextable as_paragraph
-#' @importFrom flextable autofit
-#' @importFrom flextable border_inner_h
-#' @importFrom flextable flextable
-#' @importFrom flextable hline
-#' @importFrom flextable italic
-#' @importFrom flextable mk_par
-#' @importFrom flextable ncol_keys
-#' @importFrom flextable nrow_part
-#' @importFrom flextable width
+#' @importFrom flextable add_footer_lines add_header_lines align as_equation
+#' @importFrom flextable as_paragraph autofit border_inner_h flextable
+#' @importFrom flextable hline italic mk_par ncol_keys nrow_part width
 #' @importFrom generics fit
 #' @importFrom ggplot2 aes aes_string geom_abline geom_bar geom_histogram geom_hline geom_point geom_qq geom_qq_line geom_smooth geom_vline ggtitle labs stat_function stat_smooth theme
 #' @importFrom modelr add_predictions add_residuals geom_ref_line mae qae rmse rsquare
@@ -44,9 +33,7 @@
 #' @importFrom stats AIC anova BIC coef confint cooks.distance deviance family fitted formula hatvalues nobs predict residuals rstandard variable.names vcov
 #' @importFrom stats coef pf
 #' @importFrom svFlow %>.%
-#' @importFrom svMisc gettext
-#' @importFrom svMisc gettextf
-#' @importFrom tabularise colformat_sci equation
-#' @importFrom tabularise para_md
+#' @importFrom svMisc gettext gettextf
+#' @importFrom tabularise colformat_sci equation para_md
 ## usethis namespace: end
 NULL
