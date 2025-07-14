a13b0fa85b736b9e6f33e214054d86968d807287
Guyliann Engels <guyliann.engels@umons.ac.be>
2025-07-14 15:16:35 +0200

    Add several tests for the .extract_infos_lm() function

--- tests/testthat/test-general.R
+++ tests/testthat/test-general.R
@@ -1,3 +1,7 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Update .po and .mo files", {
+  # Update .po and .mo files (only test in the source package, not R CMD check)
+  if (file.exists("../../DESCRIPTION")) {# This is the source of the package
+    cat("\nCompiling .po files...\n")
+    res <- try(tools::update_pkg_po("../.."), silent = TRUE)
+  }
 })
